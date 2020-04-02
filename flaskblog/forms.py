22ee1b06dddf64ddf58a4d96e1025c2122e72d74
Tushar Baliya <TUSHAR.BALIYA@pb.com>
2020-04-02 20:37:15 +0530

    User Authentication

---
+++ flaskblog/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
+class RegistrationForm(FlaskForm):
+	username = StringField('UserName', validators=[DataRequired(), Length(min=2, max=20)]) 
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators = [DataRequired()])
+	confirm_password = PasswordField('Password', validators = [DataRequired(), EqualTo('password')])
+	submit = SubmitField('Sign Up')
+	
+	def validate_username(self,username):
+		user = User.query.filter_by(username=username.data).first()
+		if user:
+			raise ValidationError('User is taken')
+			
+	def validate_email(self,email):
+		email = User.query.filter_by(email=email.data).first()
+		if email:
+			raise ValidationError('Email is taken')
+	
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators = [DataRequired(), Email()])
+	password = PasswordField('Password', validators = [DataRequired()])
+	remember = BooleanField('Remember Me')
+	submit = SubmitField('Login')
